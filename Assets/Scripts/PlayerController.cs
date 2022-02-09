b63b91796fd96fd1a32ff367da29fc72cdbed422
fungifurball <bejcindy@gmail.com>
2022-02-09 07:04:26 -0500

    four controllers done!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     int playerN;
     float runSpeed;
     Rigidbody rb;
+    string horizontal, vertical;
+    float x, z, s;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +25,23 @@ public class PlayerController : MonoBehaviour
         {
             case PlayerNumber.Player1:
                 playerN = 1;
+                horizontal = "Horizontal1";
+                vertical = "Vertical1";
                 break;
             case PlayerNumber.Player2:
                 playerN = 2;
+                horizontal = "Horizontal2";
+                vertical = "Vertical2";
                 break;
             case PlayerNumber.Player3:
                 playerN = 3;
+                horizontal = "Horizontal3";
+                vertical = "Vertical3";
                 break;
             case PlayerNumber.Player4:
                 playerN = 4;
+                horizontal = "Horizontal4";
+                vertical = "Vertical4";
                 break;
         }
     }
@@ -39,17 +49,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x, z, s;
-        if(Input.GetKey(KeyCode.Joystick1Button1))
+        //float x, z, s;
+        switch (playerN)
         {
-            s = runSpeed;
+            case 1:
+                    if (Input.GetKey(KeyCode.Joystick1Button1))
+                {
+                    s = runSpeed;
+                }
+                else
+                {
+                    s = speed;
+                }
+                break;
+            case 2:
+                if (Input.GetKey(KeyCode.Joystick2Button1))
+                {
+                    s = runSpeed;
+                }
+                else
+                {
+                    s = speed;
+                }
+                break;
+            case 3:
+                if (Input.GetKey(KeyCode.Joystick3Button1))
+                {
+                    s = runSpeed;
+                }
+                else
+                {
+                    s = speed;
+                }
+                break;
+            case 4:
+                if (Input.GetKey(KeyCode.Joystick4Button1))
+                {
+                    s = runSpeed;
+                }
+                else
+                {
+                    s = speed;
+                }
+                break;
         }
-        else
-        {
-            s = speed;
-        }
-        x = Input.GetAxis("Horizontal") * s;
-        z = Input.GetAxis("Vertical") * s;
+
+        x = Input.GetAxis(horizontal) * s;
+        z = Input.GetAxis(vertical) * s;
         rb.velocity = new Vector3(x, 0, z);
         Vector3 direction = new Vector3(x, 0, z).normalized;
         transform.rotation = Quaternion.LookRotation(direction, Vector3.forward);
