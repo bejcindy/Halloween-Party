2a2c5f84633c47c002a6b181dd8339ac7ceb0471
Cindy Fan <fanj696@newschool.edu>
2022-05-19 14:06:45 -0400

    audio effects half done

---
+++ Assets/Scripts/Happen2Play1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Happen2Play1 : MonoBehaviour
+{
+    public AudioClip bumpSound;
+
+    AudioSource aud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
